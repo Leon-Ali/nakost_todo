12bfd5a79e87719e5f9a2d0569145510b06cb24c
Lom-Ali <lomaliyakubov@gmail.com>
2022-08-24 10:54:01 +0300

    Add Task model

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from users.models import TodoUser
+
+
+class Task(models.Model):
+    user = models.ForeignKey(TodoUser, on_delete=models.CASCADE)
+    finished = models.BooleanField(default=False)
+    description = models.CharField(max_length=250)
+    date = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
