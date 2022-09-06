674c593224a280bf252f34486d71e95869276dc4
Lom-Ali <lomaliyakubov@gmail.com>
2022-09-06 12:21:39 +0300

    Add TasksCompleteView

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('tasks/', views.TaskCreateView.as_view(), name='tasks'),
+    path('tasks/complete/', views.TasksCompleteView.as_view(), name='tasks_complete'),
 ]
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,4 +1,8 @@
-from rest_framework import generics
+from typing import List
+
+from rest_framework import generics, status
+from rest_framework.response import Response
+
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .serializers import TaskSerializer
@@ -12,6 +16,26 @@ class TaskCreateView(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['user', 'date']
+    filterset_fields = ['user', 'date']  # todo add finished filter
+
+
+class TasksCompleteView(generics.CreateAPIView):
+    """
+    API endpoint for completing tasks by ids.
+    """
+    serializer_class = TaskSerializer
+
+    def get_queryset(self, ids=List[int]):
+        return Task.objects.filter(id__in=ids)
+
+    def create(self, request, *args, **kwargs):
+        instances = self.get_queryset(request.data.get('ids'))
+
+        for instance in instances:
+            instance.finished = True
+
+        Task.objects.bulk_update(instances, ['finished'])
+
+        return Response(status=status.HTTP_201_CREATED)
 
 
